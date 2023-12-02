4eb129932c0fa2d0e12ff1afae52f2c3cc72e976
Rory Bradford <rory.bradford@hullabalook.com>
2023-12-02 17:10:24 +0000

    Tidy up bash

--- 1/part2.bash
+++ 1/part2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-convert_to_number() {
+to_digit() {
 	case $1 in
 		one) echo 1 ;;
 		two) echo 2 ;;
@@ -19,22 +19,18 @@ words=(
 	one two three four five six seven eight nine
 )
 
-calculate() {
-	local total=0
+total=0
 
-	while read -r line; do
-		for word in ${words[@]}; do
-			line="$(echo "$line" | sed -r "s/($word)/${word}$(convert_to_number "$word")${word}/g" | tr -d '[[:space:]]')"
-		done
+while read -r line; do
+	for w in ${words[@]}; do
+		line="$(echo "$line" | sed -r "s/($w)/${w}$(to_digit "$w")${w}/g" | tr -d '[[:space:]]')"
+	done
 
-		nums="$(echo $line | grep -Po "\d+")"
-		first="${nums:0:1}"
-		last="${nums:(-1)}"
-		result="${first}${last}"
-		total="$(( total + $result ))"
-	done < input.txt
+	nums="$(echo $line | grep -Po "\d+")"
+	first="${nums:0:1}"
+	last="${nums:(-1)}"
+	result="${first}${last}"
+	total="$(( total + $result ))"
+done < input.txt
 
-	echo $total
-}
-
-calculate
+echo $total
